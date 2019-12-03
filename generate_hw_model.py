de56570b39c8b7db6f355c815f159b5abf39e1f9
Adam Rempter <arempter@gmail.com>
2019-12-03 14:59:06 +0100

    added model from tf docs

---
+++ generate_hw_model.py
@@ -0,0 +1,56 @@
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.constraints import maxnorm
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.datasets import cifar10
+from keras import Input, layers, Model, optimizers, utils
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+y_train = utils.to_categorical(y_train, 10)
+y_test = utils.to_categorical(y_test, 10)
+
+
+print('Train: X=%s, y=%s' % (x_train.shape, y_train.shape))
+print('Test: X=%s, y=%s' % (x_test.shape, y_test.shape))
+
+inputs = Input(shape=(32, 32, 3), name='img')
+x = layers.Conv2D(32, 3, activation='relu')(inputs)
+x = layers.Conv2D(64, 3, activation='relu')(x)
+block_1_output = layers.MaxPooling2D(3)(x)
+
+x = layers.Conv2D(64, 3, activation='relu', padding='same')(block_1_output)
+x = layers.Conv2D(64, 3, activation='relu', padding='same')(x)
+block_2_output = layers.add([x, block_1_output])
+
+x = layers.Conv2D(64, 3, activation='relu', padding='same')(block_2_output)
+x = layers.Conv2D(64, 3, activation='relu', padding='same')(x)
+block_3_output = layers.add([x, block_2_output])
+
+x = layers.Conv2D(64, 3, activation='relu')(block_3_output)
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dense(256, activation='relu')(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(10, activation='softmax')(x)
+
+model = Model(inputs, outputs, name='toy_resnet')
+
+epochs = 10
+seed = 11
+# sparse_categorical_crossentropy / binary_crossentropy / categorical_crossentropy
+model.compile(loss='categorical_crossentropy', optimizer=optimizers.RMSprop(1e-3), metrics=['accuracy'])
+
+#numpy.random.seed(seed)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=64)
+
+print(model.summary())
+
+scores = model.evaluate(x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+model.save('./tf_hw_model.h5')
